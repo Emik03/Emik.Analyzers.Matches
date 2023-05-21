3cff48b248b6b30c90d4b5173dcac6c52fd1cb3c
Emik <emik.lakunishok@gmail.com>
2023-05-21 11:56:03 +0200

    Revert "Use incrementor"
    This reverts commit 5199bfa2cdc2fd732be8e1508ae13211b2628f11.

--- Source/Descriptors.cs
+++ Source/Descriptors.cs
@@ -4,10 +4,9 @@ namespace Emik.Analyzers.Matches;
 /// <summary>All of the descriptors of this library.</summary>
 public static class Descriptors
 {
-    static int s_id;
-
     /// <summary>Gets the descriptor describing that the argument failed to pass the regex.</summary>
     public static DiagnosticDescriptor Eam001 { get; } = Make(
+        1,
         DiagnosticSeverity.Error,
         "Argument fails regex test",
         "The argument does not match the regex specified by the parameter.",
@@ -17,6 +16,7 @@ public static class Descriptors
     /// <summary>Gets the descriptor describing that the argument is not a constant.</summary>
     public static DiagnosticDescriptor Eam002 { get; } =
         Make(
+            2,
             DiagnosticSeverity.Warning,
             "Non-constant argument may fail regex test",
             "The argument may not match the regex specified by the parameter.",
@@ -26,6 +26,7 @@ public static class Descriptors
     /// <summary>Gets the descriptor describing that the regex took to long to process.</summary>
     public static DiagnosticDescriptor Eam003 { get; } =
         Make(
+            3,
             DiagnosticSeverity.Info,
             "Argument regex test took too long",
             "The argument took too long to validate on whether it matches the regex specified by the parameter.",
@@ -35,6 +36,7 @@ public static class Descriptors
     /// <summary>Gets the descriptor describing that this capture group cannot exist.</summary>
     public static DiagnosticDescriptor Eam004 { get; } =
         Make(
+            4,
             DiagnosticSeverity.Error,
             "Capture group doesn't exist",
             $"This will cause an {nameof(ArgumentOutOfRangeException)} at runtime.",
@@ -44,6 +46,7 @@ public static class Descriptors
     /// <summary>Gets the descriptor describing that this capture group cannot exist.</summary>
     public static DiagnosticDescriptor Eam005 { get; } =
         Make(
+            5,
             DiagnosticSeverity.Warning,
             "Non-constant argument may have wrong number of capture groups",
             $"This may cause an {nameof(ArgumentOutOfRangeException)} at runtime.",
@@ -77,13 +80,14 @@ public static class Descriptors
         };
 
     static DiagnosticDescriptor Make(
+        byte id,
         DiagnosticSeverity severity,
         string title,
         string description,
         string messageFormat
     )
     {
-        var index = (++s_id).ToString().PadLeft(3, '0');
+        var index = id.ToString().PadLeft(3, '0');
 
         return new(
             $"EAM{index}",
